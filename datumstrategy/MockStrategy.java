2b6c9b0b97428a1b5b9c4b2d2a70ec9aaf792339
Norbert Rösch <Norbert.Roesch@kit.edu>
2019-01-16 14:11:13 +0100

    Added class 'Mockstrategy'

---
+++ datumstrategy/MockStrategy.java
@@ -0,0 +1,21 @@
+package datumstrategy;
+
+import coordinates.GeographicCoordinateInterface;
+import datumstrategy.TransformationStrategy;
+
+public class MockStrategy extends TransformationStrategy {
+
+	/**
+	 * @class MocStrategy
+	 * @brief contains the implementation of the MockStrategy
+	 * @author Stütz, Huang, Weisbrich
+	 * @remark 
+	 * @version 0.1
+	 */	
+	
+	
+	@Override
+	protected void transform(GeographicCoordinateInterface geographicCoordinate) {
+		// Do nothing!	
+	}
+}
