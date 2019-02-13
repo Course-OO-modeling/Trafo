d10d0c492e0b3f183c6df216082367f8f9d2c5ef
Norbert Rösch <Norbert.Roesch@kit.edu>
2019-02-13 16:03:45 +0100

    First test for MockStrategy

--- test/datumstrategy/GetAppropriateTransformationAlgorithmTest.java
+++ test/datumstrategy/GetAppropriateTransformationAlgorithmTest.java
@@ -3,13 +3,28 @@ package test.datumstrategy;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import coordinates.GeographicCoordinateInterface;
+import datumstrategy.GetAppropriateTransformationAlgorithm;
+import datumstrategy.TransformationStrategy;
+import params.ControlParms;
 
 public class GetAppropriateTransformationAlgorithmTest {
 
 	@Test
 	public void testSpatialSimilarityTransformationInfin() {
+		ControlParms control = ControlParms.getInstance();
+		control.setKindoftrafo(null);
+		GeographicCoordinateInterface geographic = new GeographicCoordinateInterface();
+		geographic.setHeight(120.);
+		geographic.setLatitude(49.);
+		geographic.setLongitude(8.);
+		TransformationStrategy dat = GetAppropriateTransformationAlgorithm.getStrategy();
+		dat.transform(geographic);
+		assertEquals(49., geographic.getLatitude(), 0.000000001);
+		assertEquals(8., geographic.getLongitude(),0.000000001);
+		assertEquals(120.,geographic.getHeight(), 0.0001);
 		
-		fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 }
